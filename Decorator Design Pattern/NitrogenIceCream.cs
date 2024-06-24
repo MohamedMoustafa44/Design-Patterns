30811ccb5ba3187cc035865282d69f1b947c552c
Mohamed Mustafa <mmustafa@futureface.sa>
2024-06-24 13:58:59 +0300

    addition type of Ice Cream is added (Decorator Design Pattern)

---
+++ Decorator Design Pattern/NitrogenIceCream.cs
@@ -0,0 +1,8 @@
+﻿namespace Decorator_Design_Pattern;
+
+public class NitrogenIceCream : IIceCream
+{
+    public decimal CalculateCost() => 5.0m;
+
+    public string Description() => "Nitrogen Ice Cream";
+}
--- Decorator Design Pattern/Program.cs
+++ Decorator Design Pattern/Program.cs
@@ -4,11 +4,16 @@ public class Program
 {
     static void Main(string[] args)
     {
-        IIceCream order = new BasicIceCream();
-        order = new Sprinkles(order);
-        order = new Chocolate(order);
-        order = new Strawberry(order);
+        IIceCream order1 = new BasicIceCream();
+        order1 = new Sprinkles(order1);
+        order1 = new Chocolate(order1);
+        order1 = new Strawberry(order1);
 
-        Console.WriteLine($"Order: {order.Description()} | Price: {order.CalculateCost()}");
+        Console.WriteLine($"Order: {order1.Description()} | Price: {order1.CalculateCost()}");
+
+        IIceCream order2 = new NitrogenIceCream();
+        order2 = new Strawberry(order2);
+
+        Console.WriteLine($"Order: {order2.Description()} | Price: {order2.CalculateCost()}");
     }
 }
